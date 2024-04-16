020063e0da909535395ec8c65e51ae8a6fac7b76
Criscubed <criscubed@gmail.com>
2024-04-16 08:07:53 -0500

    better commit

--- bridgedb/public/index.js
+++ bridgedb/public/index.js
@@ -6,31 +6,6 @@ getBtn.addEventListener('click', getInfo);
 
 async function getInfo(e) {
     e.preventDefault();
-    console.log("WHAT IS HAPPENING")
     window.location.href = baseUrl + input.value;
-    // e.preventDefault();
-    // if (input.value == '') { return };
-    // const res = await fetch(baseUrl + 'redirect/' + input.value,
-    // {
-    //     method: 'GET'
-    // });
-    // const data = await res.json()
-    // window.location = baseUrl + data.redirectUrl
+
 }
-
-
-// async function postInfo(e) {
-//     e.preventDefault();
-//     if (input.value == '') { return }
-//     console.log("trying to redirect")
-//     const res = await fetch(baseUrl,
-//     {
-//         method: 'POST',
-//         headers: {
-//             "Content-Type": 'application/json'
-//         },
-//         body: JSON.stringify({
-//             parcel: input.value
-//         })
-//     })
-// }
--- bridgedb/script.js
+++ bridgedb/script.js
@@ -4,7 +4,6 @@ const sqlite3 = require('sqlite3')
 
 const port = 5500
 
-
 // you need to run: 
 // npm run dev 
 // to get this working
@@ -13,7 +12,6 @@ const port = 5500
 app.set('view engine', 'ejs')
 // express.js serves the files in the public folder to user
 app.use(express.static('public'));
-app.use(express.json());
 
 // any text sent as a parameter to the base url gets turned into a database query 
 // to-do: stop users from sending too many database requests
@@ -21,41 +19,21 @@ app.get('/:id', (req, res) => {
     querydb(req.params.id, getResponse, res);
 })
 
-app.get('/redirect/:id', (req, res) => {
-    // const { id } = req.params;
-    // console.log(id);
-    // res.status(200);
-    //res.redirect(301, '/' + req.params.id);
-    res.status(201).json({
-        redirectUrl: '/' + req.params.id
-    });
-})
-
-// app.post('/', (req, res) => {
-//     const { parcel } = req.body
-//     console.log(parcel)
-//     if(!parcel) {
-//         return res.status(400).send({status: failed})
-//     }
-//     res.redirect('/' + parcel)
-//     //res.status(200).send({status: 'recieved'})
-// })
-
 app.listen(port, () => console.log(`server has started on port: ${port}`));
 
 // callback method used so that res.render() gets called after the database query
-function getResponse(res, pt_information){
+function getResponse(res, pt_information) {
     //json in the response should have data from the database
     res.render("index", pt_information)
 }
 
 
-function querydb(pt_number, callback, res){
+function querydb(pt_number, callback, res) {
     // open the database
-    let obj = {null: null};
+    let obj = { null: null };
     let db = new sqlite3.Database('./bridge.db', sqlite3.OPEN_READONLY, (err) => {
         if (err) {
-        console.error(err.message);
+            console.error(err.message);
         }
         console.log('Connected to the bridge database.');
     });
@@ -64,7 +42,7 @@ function querydb(pt_number, callback, res){
     db.serialize(() => {
         db.each('SELECT * FROM bridge WHERE pt_number = "' + pt_number + '"', (err, row) => {
             if (err) {
-            console.error(err.message);
+                console.error(err.message);
             }
             //row contains information from the database
             obj = row;
@@ -74,11 +52,11 @@ function querydb(pt_number, callback, res){
     // close connection and call the callback function
     db.close((err) => {
         if (err) {
-        console.error(err.message);
+            console.error(err.message);
         }
         console.log('Closed the database connection.');
         //calback should be getResponse(res, obj)
         callback(res, obj);
     });
-    
+
 }
